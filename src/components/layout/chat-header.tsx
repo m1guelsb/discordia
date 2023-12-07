0579c31ee0e7f089a400702fb5b51923597f452e
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-12-06 23:59:49 -0300

    [feat] add avatar in chat header

--- src/components/layout/chat-header.tsx
+++ src/components/layout/chat-header.tsx
@@ -1,6 +1,8 @@
 import { Hash, Menu } from 'lucide-react'
 import React from 'react'
 import { MobileToggle } from './mobile-toggle'
+import { Avatar } from '../ui/avatar'
+import { UserAvatar } from '../ui/user-avatar'
 
 interface ChatHeaderProps {
   serverId: string
@@ -16,10 +18,13 @@ export const ChatHeader = ({
   imageUrl,
 }: ChatHeaderProps) => {
   return (
-    <div className="font-semibold px-3 flex items-center h-12 border-b-2 border-background3">
+    <div className="font-semibold px-3 flex items-center gap-2 h-12 border-b-2 border-background3">
       <MobileToggle serverId={serverId} />
       {type === 'channel' && (
         <Hash className="w-5 h-5 text-muted-foreground mr-2" />
+      )}
+      {type === 'conversation' && (
+        <UserAvatar className="h-6 w-6 md:h-8 md:w-8" src={imageUrl} />
       )}
       <p>{name}</p>
     </div>
