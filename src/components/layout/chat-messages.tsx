c817917b6fe4e847fc0f5092e535b595f57f1712
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2024-01-18 11:45:19 -0300

    [feat] add load more messages button

--- src/components/layout/chat-messages.tsx
+++ src/components/layout/chat-messages.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Fragment } from 'react'
+import { Fragment, useRef, ElementRef } from 'react'
 import { format } from 'date-fns'
 import { Loader2, ServerCrash } from 'lucide-react'
 import { Member, Message, Profile } from '@prisma/client'
@@ -42,6 +42,9 @@ export const ChatMessages = ({
   const addKey = `chat:${chatId}:messages`
   const updateKey = `chat:${chatId}:messages:update`
 
+  const chatRef = useRef<ElementRef<'div'>>(null)
+  const bottomRef = useRef<ElementRef<'div'>>(null)
+
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
       queryKey,
@@ -71,14 +74,29 @@ export const ChatMessages = ({
   }
 
   return (
-    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-      <div className="flex-1" />
-      <ChatWelcome type={type} name={name} />
+    <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
+      {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && <ChatWelcome type={type} name={name} />}
 
+      {/* //add infinite scroll */}
+      {hasNextPage && (
+        <div className="h-5 flex items-center justify-center">
+          {isFetchingNextPage ? (
+            <Loader2 className="h-5 w-5 text-muted-foreground animate-spin my-4" />
+          ) : (
+            <button
+              onClick={() => fetchNextPage()}
+              className="h-5 text-xs text-muted-foreground hover:underline"
+            >
+              Load more messages
+            </button>
+          )}
+        </div>
+      )}
       <div className="flex flex-col-reverse mt-auto">
         {data?.pages.map((group, i) => (
           <Fragment key={i}>
-            {group.items.map((message: MessageWithMemberWithProfile) => (
+            {group?.items?.map((message: MessageWithMemberWithProfile) => (
               <ChatItem
                 key={message.id}
                 id={message.id}
@@ -96,6 +114,7 @@ export const ChatMessages = ({
           </Fragment>
         ))}
       </div>
+      <div ref={bottomRef} />
     </div>
   )
 }
