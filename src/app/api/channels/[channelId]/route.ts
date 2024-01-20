40801aad2b26eba6842843742b38d85129482a07
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2024-01-20 11:44:20 -0300

    [fix] add npm package lock

--- src/app/api/channels/[channelId]/route.ts
+++ src/app/api/channels/[channelId]/route.ts
@@ -1,6 +1,7 @@
+import { NextResponse } from 'next/server'
+import { MemberRole } from '@prisma/client'
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
-import { NextResponse } from 'next/server'
 
 export async function DELETE(
   req: Request,
@@ -24,7 +25,7 @@ export async function DELETE(
           some: {
             profileId: profile.id,
             role: {
-              in: ['ADMIN', 'MODERATOR'],
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
             },
           },
         },
@@ -74,7 +75,7 @@ export async function PATCH(
           some: {
             profileId: profile.id,
             role: {
-              in: ['ADMIN', 'MODERATOR'],
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
             },
           },
         },
